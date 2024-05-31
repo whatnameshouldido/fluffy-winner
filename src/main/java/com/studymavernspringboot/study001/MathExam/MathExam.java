87fbfef5f390abbf19a840f2e0be5c98674426bb
whatnameshouldido <pobyjun@naver.com>
2024-05-31 21:49:37 +0900

    add mathTest002
    add mathTest002

--- src/main/java/com/studymavernspringboot/study001/MathExam/MathExam.java
+++ src/main/java/com/studymavernspringboot/study001/MathExam/MathExam.java
@@ -10,4 +10,18 @@ public class MathExam {
         }
         return n1 - n2;
     }
+
+    public int mathTest002(int n1, int n2) throws Exception{
+        if (n1 < 0 || n1 > 100) {
+            throw new Exception("num1 값은 0 ~ 100 사이의 값이어야 합니다.");
+        }
+        if (n2 < 0 || n2 > 100) {
+            throw new Exception("num2 값은 0 ~ 100 사이의 값이어야 합니다.");
+        }
+        return n1 * n2;
+    }
+
+    public int mathTest004(int n1, int n2) throws Exception{
+       
+        return n1 * n2;
 }
--- src/test/java/com/studymavernspringboot/study001/exam001Test.java
+++ src/test/java/com/studymavernspringboot/study001/exam001Test.java
@@ -1,7 +1,9 @@
 package com.studymavernspringboot.study001;
 
+import com.studymavernspringboot.study001.MathExam.MathExam;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class exam001Test {
@@ -11,31 +13,34 @@ public class exam001Test {
 //        int num1 = -50001;
 //        int num2 = 3;
 //        int expected = -50004;
+        MathExam math = new MathExam();
         // when     테스트를 실시
 //        int actual = subTest001(num1, num2);
         // then     실제값과 기대값을 비교해서 테스트한다.
-//        assertThat(-1).isEqualTo(subTest001(2, 3));
-//        assertThat(98).isEqualTo(subTest001(100, 2));
-//        assertThat(11).isEqualTo(subTest001(18, 7));
-//        assertThat(29).isLessThan(subTest001(100, 70));
-//        assertThat(301).isGreaterThan(subTest001(1000, 700));
-//        assertThat(actual).isEqualTo(expected).describedAs("실제값과 다름");
+        assertThat(-1).isEqualTo(math.subTest001(2, 3));
+        assertThat(98).isEqualTo(math.subTest001(100, 2));
+        assertThat(11).isEqualTo(math.subTest001(18, 7));
+        assertThat(29).isLessThan(math.subTest001(100, 70));
+        assertThat(301).isGreaterThan(math.subTest001(1000, 700));
 
         //Exception 예외가 발생해야지만 정상 동작으로 인식한다.
-        assertThatThrownBy(() -> subTest001(70000, 7)).isInstanceOf(Exception.class);
+        assertThatThrownBy(() -> math.subTest001(70000, 7)).isInstanceOf(Exception.class);
         //RuntimeException 예외가 발생해야지만 정상 동작으로 인신한다.
         Throwable exception = assertThrows(RuntimeException.class, () -> {
-            subTest001(2330, 50002);
+            math.subTest001(2330, 50002);
         });
         System.out.println(exception.toString());
     }
-    int subTest001(int n1, int n2) throws Exception{
-        if(n1 < -50000 || n1 > 50000) {
-            throw new Exception("num1 값은 -50000보다 작거나, num1 값은 50000보다 큽니다.");
-        }
-        if (n2 < -50000 || n2 > 50000) {
-            throw new RuntimeException("num2 값은 -50000보다 작거나, num2 값은 50000보다 큽니다.");
-        }
-        return n1 - n2;
+
+    @Test
+    public void multiply001() throws Exception {
+        //given
+        MathExam math = new MathExam();
+        //when
+        //then
+        assertThat(12).isEqualTo(math.mathTest002(3, 4));
+        assertThat(513).isEqualTo(math.mathTest002(27, 19));
+        assertThatThrownBy(() -> math.mathTest002(-1, 10)).isInstanceOf(Exception.class);
+        assertThatThrownBy(() -> math.mathTest002(10, 101)).isInstanceOf(Exception.class);
     }
 }
